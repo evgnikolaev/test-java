c075c9c10fffeee382fd60374a1ffaa70159031f
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2025-07-01 10:37:33 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -57,25 +57,62 @@ switch
 
 ЦИКЛЫ
 
+    while(условие) оператор;
+
+            while (kol < N) {
+            }
 
 
 
+    for(инициализация; условие_повторения; модификация)   оператор;
+
+            for (int kol = 0; kol < N; kol++) {
+            }
+
+
+
+    do  оператор; while (условие);
+
+            do {
+            } while (x != 0);
+
+
+
+
+
+    for (тип_данных имя_временное : имя_коллекции)    операторы; (только для чтения, элементы массива менять не можем)
+
+            double[] b = {3.5, 6.3, 8.4};
+            for (double x : b) {
+                System.out.print(x + " ");
+            }
 
 
 
  */
 
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    static final int N = 10;
+
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
         Scanner scan = new Scanner(System.in);
-        int month = scan.nextInt();
+        int height = scan.nextInt();
+        for(int i = 1; i <= height; i++) {
+            for(int j = 1; j <= height; j++) {
+                System.out.print(i*j);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
 
     }
 }
-
 
 
 
